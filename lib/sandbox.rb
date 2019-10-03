380bc34f2c96e544c371277d91f31c3121a21daf
Jingjing Duan <jingjing.duan@airbnb.com>
2019-10-03 09:04:37 -0700

    multiline strings

--- lib/sandbox.rb
+++ lib/sandbox.rb
@@ -2,10 +2,16 @@ require "sandbox/version"
 
 module Sandbox
   def self.hello
-    puts "hello world!"
+    name = "john"
+    multi_line_str = <<~MSG
+      This is the first line.
+      This is the second line.
+      #{name}
+    MSG
+    puts multi_line_str
   end
 
-  private_class_method :hello
+  # private_class_method :hello
 end
 
 puts "hi"
